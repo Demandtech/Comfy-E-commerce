961bffbfd3ec39485b5f3a3b8a19dd8473c3ceb8
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-02-07 11:27:21 +0100

    context and reducer setup for the above functionality

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -19,7 +19,7 @@ const CartContent = () => {
         <Link to={'/products'} className='link-btn'>
           continue shopping
         </Link>
-        <button type='button' className='clear-btn link-btn'>clear shopping cart</button>
+        <button type='button' className='clear-btn link-btn' onClick={clearCart}>clear shopping cart</button>
       </div>
       <CartTotals />
     </Wrapper>
