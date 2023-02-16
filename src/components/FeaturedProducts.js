6ec096d94493940fb62da0ef7b8d09f36a7906b9
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-02-17 00:40:57 +0100

    fix: correct feature spelling

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -21,7 +21,7 @@ const FeaturedProducts = () => {
   return (
     <Wrapper className='section'>
       <div className='title'>
-        <h2>featired products</h2>
+        <h2>featured products</h2>
         <div className='underline'></div>
       </div>
       <div className='section-center featured'>
